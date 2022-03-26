b8e1f653c6c1c71313c9558577783ac58401b440
o.kotyay <o.kotyay@ideil.com>
2022-03-26 12:02:24 +0200

    php sodium and imagick extensions

--- workspace/startup.sh
+++ workspace/startup.sh
@@ -4,9 +4,9 @@
 # $HOME/.phpenv/versions/7.4.27/etc/init.d/php-fpm start
 # $HOME/.phpenv/versions/8.0.16/etc/init.d/php-fpm start
 # $HOME/.phpenv/versions/8.1.3/etc/init.d/php-fpm start
-$HOME/.asdf/installs/php/7.4.28/sbin/php-fpm
-$HOME/.asdf/installs/php/8.0.16/sbin/php-fpm
-$HOME/.asdf/installs/php/8.1.3/sbin/php-fpm
+$ASDF_DATA_DIR/installs/php/7.4.28/sbin/php-fpm
+$ASDF_DATA_DIR/installs/php/8.0.16/sbin/php-fpm
+$ASDF_DATA_DIR/installs/php/8.1.3/sbin/php-fpm
 
 # Start nginx in foreground
 nginx
