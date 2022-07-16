4df3e18be6b71ee4a845f24c96ee371f04ddfa02
o.kotyay <o.kotyay@ideil.com>
2022-07-17 00:54:29 +0300

    normalized asdf installation

--- workspace/startup.sh
+++ workspace/startup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Install nodejs
+bash /opt/install-nodejs.sh ${ASDF_DATA_DIR}/installs/nodejs
+asdf global nodejs 16.14.0
+
+# Install php
+bash /opt/install-php.sh ${ASDF_DATA_DIR}/installs/php
+asdf global php 8.1.3
+
+# Configuration asdf for web user
+cp /root/.tool-versions /home/web/.tool-versions
+chown -R web: /home/web/.tool-versions
+asdf reshim
+
 # Start php-fpm in foreground
-# $HOME/.phpenv/versions/7.4.27/etc/init.d/php-fpm start
-# $HOME/.phpenv/versions/8.0.16/etc/init.d/php-fpm start
-# $HOME/.phpenv/versions/8.1.3/etc/init.d/php-fpm start
 $ASDF_DATA_DIR/installs/php/7.4.28/sbin/php-fpm
 $ASDF_DATA_DIR/installs/php/8.0.16/sbin/php-fpm
 $ASDF_DATA_DIR/installs/php/8.1.3/sbin/php-fpm
