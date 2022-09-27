560c48b9e023295de1a949b19f53c375b22d2243
Oleh Kotyay <o.kotyay@ideil.com>
2022-09-27 14:03:42 +0300

    php asdf updated, php5.6 enabled

--- workspace/install-php.sh
+++ workspace/install-php.sh
@@ -1,98 +1,88 @@
 #!/bin/bash
 
-# # installing php-7.1.33 if not installed
-# if [ ! -d "${1}/7.1.33" ]; then
-#     asdf install php 7.1.33
+# installing php-5.6.40 if not installed
+if [ ! -d "${1}/5.6.40" ]; then
+    asdf php update php-build
 
-#     cp ${1}/7.1.33/php-fpm.conf.default ${1}/7.1.33/php-fpm.conf && \
-#         cp ${1}/7.1.33/php-fpm.d/www.conf.default ${1}/7.1.33/php-fpm.d/www.conf && \
-#         sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9071/' ${1}/7.1.33/php-fpm.d/www.conf && \
-#         sed -i 's/user = www-data/user = web/' ${1}/7.1.33/php-fpm.d/www.conf && \
-#         sed -i 's/group = www-data/group = web/' ${1}/7.1.33/php-fpm.d/www.conf
+    PHP_BUILD_CONFIGURE_OPTS="--with-gettext" asdf install php 5.6.40
 
-#     cp -r php/php7.1.ini ${1}/7.1.33/conf.d/php.ini
+    sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9056/' ${1}/5.6.40/etc/php-fpm.d/www.conf && \
+    sed -i 's/user = nobody/user = web/' ${1}/5.6.40/etc/php-fpm.d/www.conf && \
+    sed -i 's/group = nobody/group = web/' ${1}/5.6.40/etc/php-fpm.d/www.conf
 
-#     asdf global php 7.1.33 && \
-#         $ASDF_DATA_DIR/shims/pecl install libsodium imagick && \
-#         echo 'extension=sodium.so' > ${1}/7.1.33/conf.d/sodium.ini && \
-#         echo 'extension=imagick.so' > ${1}/7.1.33/conf.d/imagick.ini
-# fi
+    asdf global php 5.6.40 && \
+        ${ASDF_DATA_DIR}/shims/pecl channel-update pecl.php.net && \
+        printf "\n" | ${ASDF_DATA_DIR}/shims/pecl install libsodium imagick redis && \
+        echo 'extension=sodium.so' > ${1}/5.6.40/etc/conf.d/sodium.ini && \
+        echo 'extension=imagick.so' > ${1}/5.6.40/etc/conf.d/imagick.ini && \
+        echo 'extension=redis.so' > ${1}/5.6.40/etc/conf.d/redis.ini
+fi
+
+# installing php-7.1.33 if not installed
+if [ ! -d "${1}/7.1.33" ]; then
+    asdf php update php-build
+
+    PHP_BUILD_CONFIGURE_OPTS="--with-gettext" asdf install php 7.1.33
+
+    sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9071/' ${1}/7.1.33/etc/php-fpm.d/www.conf && \
+    sed -i 's/user = nobody/user = web/' ${1}/7.1.33/etc/php-fpm.d/www.conf && \
+    sed -i 's/group = nobody/group = web/' ${1}/7.1.33/etc/php-fpm.d/www.conf
+
+    asdf global php 7.1.33 && \
+        ${ASDF_DATA_DIR}/shims/pecl channel-update pecl.php.net && \
+        printf "\n" | ${ASDF_DATA_DIR}/shims/pecl install libsodium imagick redis && \
+        echo 'extension=sodium.so' > ${1}/7.1.33/etc/conf.d/sodium.ini && \
+        echo 'extension=imagick.so' > ${1}/7.1.33/etc/conf.d/imagick.ini && \
+        echo 'extension=redis.so' > ${1}/7.1.33/etc/conf.d/redis.ini
+fi
 
 # installing php-7.4.28 if not installed
 if [ ! -d "${1}/7.4.28" ]; then
-    PHP_CONFIGURE_OPTIONS="--with-openssl --with-curl --with-zlib --with-readline --with-gettext --with-xsl" asdf install php 7.4.28
+    asdf php update php-build
 
-    cp ${1}/7.4.28/php-fpm.conf.default ${1}/7.4.28/php-fpm.conf && \
-        cp ${1}/7.4.28/php-fpm.d/www.conf.default ${1}/7.4.28/php-fpm.d/www.conf && \
-        sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9074/' ${1}/7.4.28/php-fpm.d/www.conf && \
-        sed -i 's/user = www-data/user = web/' ${1}/7.4.28/php-fpm.d/www.conf && \
-        sed -i 's/group = www-data/group = web/' ${1}/7.4.28/php-fpm.d/www.conf
+    PHP_BUILD_CONFIGURE_OPTS="--with-gettext --with-sodium" asdf install php 7.4.28
 
-    cp ${ASDF_DATA_DIR}/php-ini/php7.4.ini ${1}/7.4.28/conf.d/php.ini
+    sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9074/' ${1}/7.4.28/etc/php-fpm.d/www.conf && \
+    sed -i 's/user = nobody/user = web/' ${1}/7.4.28/etc/php-fpm.d/www.conf && \
+    sed -i 's/group = nobody/group = web/' ${1}/7.4.28/etc/php-fpm.d/www.conf
 
     asdf global php 7.4.28 && \
-        $ASDF_DATA_DIR/shims/pecl channel-update pecl.php.net && \
-        printf "\n" | $ASDF_DATA_DIR/shims/pecl install libsodium imagick redis xdebug && \
-        echo 'extension=sodium.so' > ${1}/7.4.28/conf.d/sodium.ini && \
-        echo 'extension=imagick.so' > ${1}/7.4.28/conf.d/imagick.ini && \
-        echo 'extension=redis.so' > ${1}/7.4.28/conf.d/redis.ini && \
-        echo 'zend_extension=xdebug' > ${1}/7.4.28/conf.d/xdebug.ini
-
-    echo '' | tee -a ${1}/7.4.28/conf.d/xdebug.ini && \
-        echo 'xdebug.mode=develop,coverage,debug,profile' | tee -a ${1}/7.4.28/conf.d/xdebug.ini && \
-        echo 'xdebug.start_with_request=yes' | tee -a ${1}/7.4.28/conf.d/xdebug.ini && \
-        echo 'xdebug.log_level=0' | tee -a ${1}/7.4.28/conf.d/xdebug.ini && \
-        echo '' | tee -a ${1}/7.4.28/conf.d/xdebug.ini
+        ${ASDF_DATA_DIR}/shims/pecl channel-update pecl.php.net && \
+        printf "\n" | ${ASDF_DATA_DIR}/shims/pecl install imagick redis && \
+        echo 'extension=imagick.so' > ${1}/7.4.28/etc/conf.d/imagick.ini && \
+        echo 'extension=redis.so' > ${1}/7.4.28/etc/conf.d/redis.ini
 fi
 
-# installing php-8.0.16 if not installed
-if [ ! -d "${1}/8.0.16" ]; then
-    asdf install php 8.0.16
+# installing php-8.0.23 if not installed
+if [ ! -d "${1}/8.0.23" ]; then
+    asdf php update php-build
 
-    cp ${1}/8.0.16/php-fpm.conf.default ${1}/8.0.16/php-fpm.conf && \
-        cp ${1}/8.0.16/php-fpm.d/www.conf.default ${1}/8.0.16/php-fpm.d/www.conf && \
-        sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9080/' ${1}/8.0.16/php-fpm.d/www.conf && \
-        sed -i 's/user = www-data/user = web/' ${1}/8.0.16/php-fpm.d/www.conf && \
-        sed -i 's/group = www-data/group = web/' ${1}/8.0.16/php-fpm.d/www.conf
+    PHP_BUILD_CONFIGURE_OPTS="--with-gettext --with-sodium" asdf install php 8.0.23
 
-    cp ${ASDF_DATA_DIR}/php-ini/php8.0.ini ${1}/8.0.16/conf.d/php.ini
+    sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9080/' ${1}/8.0.23/etc/php-fpm.d/www.conf && \
+    sed -i 's/user = nobody/user = web/' ${1}/8.0.23/etc/php-fpm.d/www.conf && \
+    sed -i 's/group = nobody/group = web/' ${1}/8.0.23/etc/php-fpm.d/www.conf
 
-    asdf global php 8.0.16 && \
-        $ASDF_DATA_DIR/shims/pecl channel-update pecl.php.net && \
-        printf "\n" | $ASDF_DATA_DIR/shims/pecl install imagick redis xdebug && \
-        echo 'extension=imagick.so' > ${1}/8.0.16/conf.d/imagick.ini && \
-        echo 'extension=redis.so' > ${1}/8.0.16/conf.d/redis.ini && \
-        echo 'zend_extension=xdebug' > ${1}/8.0.16/conf.d/xdebug.ini
-
-    echo '' | tee -a ${1}/8.0.16/conf.d/xdebug.ini && \
-        echo 'xdebug.mode=develop,coverage,debug,profile' | tee -a ${1}/8.0.16/conf.d/xdebug.ini && \
-        echo 'xdebug.start_with_request=yes' | tee -a ${1}/8.0.16/conf.d/xdebug.ini && \
-        echo 'xdebug.log_level=0' | tee -a ${1}/8.0.16/conf.d/xdebug.ini && \
-        echo '' | tee -a ${1}/8.0.16/conf.d/xdebug.ini
+    asdf global php 8.0.23 && \
+        ${ASDF_DATA_DIR}/shims/pecl channel-update pecl.php.net && \
+        printf "\n" | ${ASDF_DATA_DIR}/shims/pecl install imagick redis && \
+        echo 'extension=imagick.so' > ${1}/8.0.23/etc/conf.d/imagick.ini && \
+        echo 'extension=redis.so' > ${1}/8.0.23/etc/conf.d/redis.ini
 fi
 
-# installing php-8.1.3 if not installed
-if [ ! -d "${1}/8.1.3" ]; then
-    PHP_CONFIGURE_OPTIONS="--with-openssl --with-curl --with-zlib --with-readline --with-gettext --with-sodium --with-xsl" asdf install php 8.1.3
+# installing php-8.1.10 if not installed
+if [ ! -d "${1}/8.1.10" ]; then
+    asdf php update php-build
 
-    cp ${1}/8.1.3/php-fpm.conf.default ${1}/8.1.3/php-fpm.conf && \
-        cp ${1}/8.1.3/php-fpm.d/www.conf.default ${1}/8.1.3/php-fpm.d/www.conf && \
-        sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9081/' ${1}/8.1.3/php-fpm.d/www.conf && \
-        sed -i 's/user = www-data/user = web/' ${1}/8.1.3/php-fpm.d/www.conf && \
-        sed -i 's/group = www-data/group = web/' ${1}/8.1.3/php-fpm.d/www.conf
+    PHP_BUILD_CONFIGURE_OPTS="--with-gettext --with-sodium" asdf install php 8.1.10
 
-    cp ${ASDF_DATA_DIR}/php-ini/php8.1.ini ${1}/8.1.3/conf.d/php.ini
+    sed -i 's/listen = 127.0.0.1:9000/listen = 127.0.0.1:9081/' ${1}/8.1.10/etc/php-fpm.d/www.conf && \
+    sed -i 's/user = nobody/user = web/' ${1}/8.1.10/etc/php-fpm.d/www.conf && \
+    sed -i 's/group = nobody/group = web/' ${1}/8.1.10/etc/php-fpm.d/www.conf
 
-    asdf global php 8.1.3 && \
-        $ASDF_DATA_DIR/shims/pecl channel-update pecl.php.net && \
-        printf "\n" | $ASDF_DATA_DIR/shims/pecl install imagick redis xdebug && \
-        echo 'extension=imagick.so' > ${1}/8.1.3/conf.d/imagick.ini && \
-        echo 'extension=redis.so' > ${1}/8.1.3/conf.d/redis.ini && \
-        echo 'zend_extension=xdebug' > ${1}/8.1.3/conf.d/xdebug.ini
-
-    echo '' | tee -a ${1}/8.1.3/conf.d/xdebug.ini && \
-        echo 'xdebug.mode=develop,coverage,debug,profile' | tee -a ${1}/8.1.3/conf.d/xdebug.ini && \
-        echo 'xdebug.start_with_request=yes' | tee -a ${1}/8.1.3/conf.d/xdebug.ini && \
-        echo 'xdebug.log_level=0' | tee -a ${1}/8.1.3/conf.d/xdebug.ini && \
-        echo '' | tee -a ${1}/8.1.3/conf.d/xdebug.ini
+    asdf global php 8.1.10 && \
+        ${ASDF_DATA_DIR}/shims/pecl channel-update pecl.php.net && \
+        printf "\n" | ${ASDF_DATA_DIR}/shims/pecl install imagick redis && \
+        echo 'extension=imagick.so' > ${1}/8.1.10/etc/conf.d/imagick.ini && \
+        echo 'extension=redis.so' > ${1}/8.1.10/etc/conf.d/redis.ini
 fi
--- workspace/startup.sh
+++ workspace/startup.sh
@@ -6,7 +6,7 @@ asdf global nodejs 16.14.0
 
 # Install php
 bash /opt/install-php.sh ${ASDF_DATA_DIR}/installs/php
-asdf global php 8.1.3
+asdf global php 8.1.10
 
 # Configuration asdf for web user
 cp /root/.tool-versions /home/web/.tool-versions
@@ -15,9 +15,11 @@ chown -R web: ${ASDF_DATA_DIR}
 asdf reshim
 
 # Start php-fpm in foreground
+${ASDF_DATA_DIR}/installs/php/5.6.40/sbin/php-fpm
+${ASDF_DATA_DIR}/installs/php/7.1.33/sbin/php-fpm
 ${ASDF_DATA_DIR}/installs/php/7.4.28/sbin/php-fpm
-${ASDF_DATA_DIR}/installs/php/8.0.16/sbin/php-fpm
-${ASDF_DATA_DIR}/installs/php/8.1.3/sbin/php-fpm
+${ASDF_DATA_DIR}/installs/php/8.0.23/sbin/php-fpm
+${ASDF_DATA_DIR}/installs/php/8.1.10/sbin/php-fpm
 
 # Trust localhttps certificates
 sudo trust anchor --store ${LOCALHTTPS_DATA}/CertificationAuthorities/default.pem
