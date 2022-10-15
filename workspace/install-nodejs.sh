5c9766633e7515960187727ae564163eac295228
o.kotyay <o.kotyay@ideil.com>
2022-10-15 13:08:42 +0300

    nodejs update

--- workspace/install-nodejs.sh
+++ workspace/install-nodejs.sh
@@ -5,17 +5,22 @@ if [ ! -d "${1}/10.24.1" ]; then
     asdf install nodejs 10.24.1
 fi
 
-# installing node-12.22.10 if not installed
-if [ ! -d "${1}/12.22.10" ]; then
-    asdf install nodejs 12.22.10
+# installing node-12.22.12 if not installed
+if [ ! -d "${1}/12.22.12" ]; then
+    asdf install nodejs 12.22.12
 fi
 
-# installing node-14.19.0 if not installed
-if [ ! -d "${1}/14.19.0" ]; then
-    asdf install nodejs 14.19.0
+# installing node-14.20.1 if not installed
+if [ ! -d "${1}/14.20.1" ]; then
+    asdf install nodejs 14.20.1
 fi
 
-# installing node-16.14.0 if not installed
-if [ ! -d "${1}/16.14.0" ]; then
-    asdf install nodejs 16.14.0
+# installing node-16.18.0 if not installed
+if [ ! -d "${1}/16.18.0" ]; then
+    asdf install nodejs 16.18.0
+fi
+
+# installing node-18.11.0 if not installed
+if [ ! -d "${1}/18.11.0" ]; then
+    asdf install nodejs 18.11.0
 fi
