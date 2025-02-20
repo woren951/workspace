7a1c8cc40d6fbc7fef0d2f46526d7783819483e4
woren951 <woren951@gmail.com>
2025-02-20 19:34:52 +0200

    update

--- workspace/startup.sh
+++ workspace/startup.sh
@@ -17,13 +17,13 @@ fi
 bash /opt/install-nodejs.sh ${ASDF_DATA_DIR}/installs/nodejs
 asdf global nodejs 20.10.0
 
-# Install asdf php plugin and php versions if not installed
-if [ ! -d "${ASDF_DATA_DIR}/plugins/php" ]; then
-    asdf plugin add php https://github.com/Tarik02/asdf-php.git
-fi
+# # Install asdf php plugin and php versions if not installed
+# if [ ! -d "${ASDF_DATA_DIR}/plugins/php" ]; then
+#     asdf plugin add php https://github.com/woren951/asdf-php.git
+# fi
 
-bash /opt/install-php.sh ${ASDF_DATA_DIR}/installs/php
-asdf global php 8.2.10
+# bash /opt/install-php.sh ${ASDF_DATA_DIR}/installs/php ${ASDF_DATA_DIR}
+# asdf global php 8.3.9
 
 # Asdf reshim
 asdf reshim
@@ -35,16 +35,18 @@ cp -r /home/web/.web-shared/. /home/web/
 sudo /usr/sbin/sshd
 
 # Start php-fpm in foreground
-sudo ${ASDF_DATA_DIR}/installs/php/5.6.40/sbin/php-fpm
-sudo ${ASDF_DATA_DIR}/installs/php/7.1.33/sbin/php-fpm
-sudo ${ASDF_DATA_DIR}/installs/php/7.4.33/sbin/php-fpm
-sudo ${ASDF_DATA_DIR}/installs/php/8.0.26/sbin/php-fpm
-sudo ${ASDF_DATA_DIR}/installs/php/8.1.13/sbin/php-fpm
-sudo ${ASDF_DATA_DIR}/installs/php/8.2.10/sbin/php-fpm
-sudo ${ASDF_DATA_DIR}/installs/php/8.3.2/sbin/php-fpm
+# sudo ${ASDF_DATA_DIR}/installs/php/5.6.40/sbin/php-fpm
+# sudo ${ASDF_DATA_DIR}/installs/php/7.1.33/sbin/php-fpm
+# sudo ${ASDF_DATA_DIR}/installs/php/7.4.33/sbin/php-fpm
+# sudo ${ASDF_DATA_DIR}/installs/php/8.0.26/sbin/php-fpm
+# sudo ${ASDF_DATA_DIR}/installs/php/8.1.29/sbin/php-fpm
+# sudo ${ASDF_DATA_DIR}/installs/php/8.2.21/sbin/php-fpm
+# sudo ${ASDF_DATA_DIR}/installs/php/8.3.9/sbin/php-fpm
+
+sudo /opt/php/buildroot/bin/php-fpm
 
 # Trust localhttps certificates
-sudo trust anchor --store ${LOCALHTTPS_DATA}/CertificationAuthorities/default.pem
+# sudo trust anchor --store ${LOCALHTTPS_DATA}/CertificationAuthorities/default.pem
 
 # Start nginx in foreground
 sudo nginx
